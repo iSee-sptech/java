86e874534c3d3495bfdf680e6915a80d290a74f9
Fabiano Souza <54280537+FabianoSouzaCotrim@users.noreply.github.com>
2022-11-12 19:57:08 -0300

    java

---
+++ java-main/isee/src/main/java/telas/jframe/isee/LoginDTO.java
@@ -0,0 +1,35 @@
+package telas.jframe.isee;
+
+
+public class LoginDTO {
+    
+    private int idUsuario;
+    private String emailUsuario;
+    private String senhaUsuario;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public String getSenhaUsuario() {
+        return senhaUsuario;
+    }
+
+    public void setSenhaUsuario(String senhaUsuario) {
+        this.senhaUsuario = senhaUsuario;
+    }
+    
+}
+
