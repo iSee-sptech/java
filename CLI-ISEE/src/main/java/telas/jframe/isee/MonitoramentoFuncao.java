0bd8da7d0b1d905568c6e84fefd9dd5104753fb2
Yuri Martins <99891817+YuriMartins@users.noreply.github.com>
2022-11-19 14:43:24 -0300

    Update MonitoramentoFuncao.java

--- CLI-ISEE/src/main/java/telas/jframe/isee/MonitoramentoFuncao.java
+++ CLI-ISEE/src/main/java/telas/jframe/isee/MonitoramentoFuncao.java
@@ -39,7 +39,7 @@ public class MonitoramentoFuncao {
         if (disco.length() > 12) {
             disco = disco.substring(0, 4);
         } else {
-            disco = disco.substring(0, 2);
+            disco = disco.substring(0, 1);
         }
 
         Long Total = dados.getTotal();
