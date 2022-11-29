c61c317ec44cc2ed23c206372aa71415c784c5ba
Fabiano Souza <54280537+FabianoSouzaCotrim@users.noreply.github.com>
2022-11-29 18:23:29 -0300

    Add files via upload

---
+++ java/isee/src/main/java/telas/jframe/isee/Conexao.java
@@ -0,0 +1,82 @@
+package telas.jframe.isee;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Conexao {
+
+    private JdbcTemplate connection;
+    private JdbcTemplate connectionMySql;
+
+    // Exemplo de configuração utilizando H2
+    // Obs. O código comentado é um exemplo de como se conectar ao mysql
+    public Conexao() {
+        BasicDataSource datasource = new BasicDataSource();
+        BasicDataSource datasourceMySql = new BasicDataSource();
+
+        datasourceMySql.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        datasource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); 
+        
+        datasource.setUrl("jdbc:sqlserver://svr-isee.database.windows.net:1433;database=isee");
+
+        datasource.setUsername("grupo8@svr-isee");
+
+        datasource.setPassword("Projetoisee123");
+
+        datasourceMySql.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        //datasourceMySql.setUrl("jdbc:mysql://localhost:3360/image_v3");
+        datasourceMySql.setUrl("jdbc:mysql://localhost:3360/?serverTimezone=UTC");
+        //datasourceMySql.setUsername("root");
+        datasourceMySql.setUsername("root");
+        datasourceMySql.setPassword("urubu100");
+        connection = new JdbcTemplate(datasource);
+        connectionMySql = new JdbcTemplate(datasourceMySql);
+    }
+
+    public JdbcTemplate getConnection() {
+        return connection;
+    }
+    
+    public JdbcTemplate getConnectionMySql() {
+        return connectionMySql;
+    }
+
+    public Connection conectaBD() {
+        Connection conn = null;
+        
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://svr-isee.database.windows.net:1433;database=isee;user=grupo8@svr-isee;password=Projetoisee123";
+            conn = DriverManager.getConnection(url);
+            
+        } catch(SQLException erro){
+            JOptionPane.showMessageDialog(null, "Conexao Login" + erro.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+    
+    public Connection conectaBDMySql() {
+        Connection conn = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3360/?serverTimezone=UTC";
+            conn = DriverManager.getConnection(url);
+            
+        } catch(SQLException erro){
+            JOptionPane.showMessageDialog(null, "Conexao Login MySql" + erro.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+}
---
+++ java/isee/src/main/java/telas/jframe/isee/GeradorDeLog.java
@@ -0,0 +1,83 @@
+package telas.jframe.isee;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class GeradorDeLog {
+
+    public static void gerarLog(String tipo) throws IOException {
+
+        LocalDateTime agora = LocalDateTime.now();
+
+        DateTimeFormatter data = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+        String dataForm = data.format(agora);
+
+        DateTimeFormatter hora = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String horaFom = hora.format(agora);
+
+        Path path = Paths.get("/home/ubuntu/Documents/logs/logs.txt");
+
+        if (!Files.exists(path)) {
+
+            Files.createDirectory(path);
+
+        }
+
+        File log = new File("/home/ubuntu/Documents/logs/logs.txt");
+
+        if (!log.exists()) {
+
+            log.createNewFile();
+
+        }
+
+        FileWriter fw = new FileWriter(log, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        if (tipo.equalsIgnoreCase("loginRealizado")) {
+            bw.write("Usuário efetuou login com sucesso" + dataForm + " - " + horaFom);
+            bw.write("__________________________________________________________________________________________");
+            bw.newLine();
+
+            bw.close();
+            fw.close();
+        } else if (tipo.equalsIgnoreCase("loginErro")) {
+            bw.write("Erro de login - Não foi possivel efetuar o login pois o email ou a senha está incorreta" + dataForm + " - " + horaFom);
+            bw.write("__________________________________________________________________________________________");
+            bw.newLine();
+
+            bw.close();
+            fw.close();
+        } else if (tipo.equalsIgnoreCase("maquinaCadastrada")) {
+            bw.write("Cadastro de maquina realizado com sucesso" + dataForm + " - " + horaFom);
+            bw.write("__________________________________________________________________________________________");
+            bw.newLine();
+
+            bw.close();
+            fw.close();
+        }else if (tipo.equalsIgnoreCase("maquinaDesligada")) {
+            bw.write("A Maquina foi desligada" + dataForm + " - " + horaFom);
+            bw.write("__________________________________________________________________________________________");
+            bw.newLine();
+
+            bw.close();
+            fw.close();
+        } else if (tipo.equalsIgnoreCase("bdErro")) {
+            bw.write("Erro no Banco - Banco de dados não conectado" + dataForm + " - " + horaFom);
+            bw.write("__________________________________________________________________________________________");
+            bw.newLine();
+
+            bw.close();
+            fw.close();
+        }
+
+    }
+
+}
